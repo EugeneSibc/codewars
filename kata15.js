3169db6211824abdf86a9d9d6934fbf964d7b2b3
eugeneman <evgensbk9@gmail.com>
2023-02-27 22:55:50 +0700

    i don't know

--- kata15.js
+++ kata15.js
@@ -15,3 +15,21 @@ describe("Tests", () => {
         Test.assertEquals(sum([1, 5.2, 4, 0, -1]), 9.2);
     });
 });*/
+
+
+function digitize(n){
+    return (n + '').split('').map(Number).reverse();
+}
+//function digitize(n) {
+//   return String(n).split('').map(Number).reverse()
+// }
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(b => !geese.includes(b));
+};
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter( bird => geese.indexOf(bird) < 0 );
+};
